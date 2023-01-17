2ac3e4b03670d360e173fda725f05cdf2d0e4207
PFonsecaFV <pfonsecafv@gmail.com>
2023-01-17 16:37:29 -0300

    Requisito 03 - Concluído

--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import {
+  // createProductImageElement,
+  // createCustomElement,
+  createProductElement,
+  // getIdFromProduct,
+  // createCartProductElement,
+} from './helpers/shopFunctions';
+
+const product = document.querySelector('.products');
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const dataStoreProducts = async () => { // Requisito 03
+  const storeProducts = document.querySelector('.products');
+  const dataProduct = await fetchProductsList('computador');
+
+  dataProduct.forEach((element) => {
+    storeProducts.appendChild(createProductElement(element));
+  });
+};
+
+dataStoreProducts();
